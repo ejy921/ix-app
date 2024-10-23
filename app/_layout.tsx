5b31d06065d9419550f635f70e8ac90ae676e472
Jinny <ejy921@gmail.com>
2024-10-22 22:03:12 -0500

    Import button component
    Top navbar init

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,42 @@
 import { Stack } from "expo-router";
+import Ionicons from '@expo/vector-icons/Ionicons'
+import { Text, StyleSheet } from 'react-native';
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={ ({ route }) => ({ 
+        headerShown: true, 
+        headerTitle: () => (
+          <Text style={styles.headerTitle}>
+            {route.name} {}
+          </Text>
+        ),
+        headerLeft: () => (<Ionicons 
+          name="menu"
+          size={30}
+          color="blue"
+          style={{ marginLeft: 10}}
+          onPress={() => alert('Menu pressed')} 
+          /> ),
+        headerRight: () => (<Ionicons
+          name="call-outline"
+          size={30}
+          color="blue"
+          style={{ marginRight: 10}}
+          onPress={() => alert('Menu pressed')}
+          /> )
+      })} />
+
       <Stack.Screen name="+not-found" />
     </Stack>
   );
 }
+
+const styles = StyleSheet.create({
+  headerTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'blue', // Customize the title color as needed
+  },
+});
